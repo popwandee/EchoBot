15d6a9beb42e9d931023f2a68d339b8963de67b4
popwandee <popwandee@gmail.com>
2018-05-25 20:32:59 +0700

    check image event

--- callback.php
+++ callback.php
@@ -23,6 +23,11 @@ $body = file_get_contents("php://input");
 $events = $bot->parseEventRequest($body, $signature);
 
 foreach ($events as $event) {
+   if ($event  instanceof \LINE\LINEBot\Event\MessageEvent\ImageMessage){
+     $reply_token = $event->getReplyToken();
+     $text = "show image";//$event->getText();
+     $bot->replyText($reply_token, $text);
+   }
     if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
         $reply_token = $event->getReplyToken();
         $text = $event->getText();
