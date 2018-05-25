e19cdbd472f4af6d1177ebb48e8e1de2dd40b418
popwandee <popwandee@gmail.com>
2018-05-25 20:19:34 +0700

    dump temp.txt

--- callback.php
+++ callback.php
@@ -1,4 +1,12 @@
 <?php // callback.php
+ob_start();
+$raw = file_get_contents('php://input');
+var_dump(json_decode($raw,1));
+$raw = ob_get_clean();
+file_put_contents('/tmp/dump.txt', $raw."\n=====================================\n", FILE_APPEND);
+
+echo "Dump temp OK";
+
 define("LINE_MESSAGING_API_CHANNEL_SECRET", '82d7948950b54381bcbd0345be0d4a2c');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'BYnvAcR40qJk4fLopvVtVozF00iUqfUjoD33tIPcnjMoXEyG3fzYSE24XRKB5lnttxPePUIHPWdylLdkROwbOESi4rQE3+oSG3njcFj7yoQuaqU27effhhF4lz6lbOfhPjD9mLvHWYZlSbeigV4ETAdB04t89/1O/w1cDnyilFU=');
 echo "ok 1";
@@ -19,19 +27,22 @@ foreach ($events as $event) {
         $reply_token = $event->getReplyToken();
         $text = $event->getText();
         switch ($text) {
-          case 'สอนฮูก': $text=$text."สอนฮูก";break;
+          case 'สอนฮูก':
+              $text=$text."สอนฮูก";
+
+              break;
           case 'stock': $text=$text.'stock price';break;
           case 'news': $text=$text.'news update';break;
-          default: 
-          $api_key="6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv";
-          $url = 'https://api.mlab.com/api/1/databases/hooqline/collections/linebot?apiKey='.$api_key.'';
-          $json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$text.'"}');
-          $data = json_decode($json);
-            foreach($data as $rec){
-              $text= $rec->answer;
-              //-----------------------
+          default:
+              $api_key="6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv";
+              $url = 'https://api.mlab.com/api/1/databases/hooqline/collections/linebot?apiKey='.$api_key.'';
+              $json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$text.'"}');
+              $data = json_decode($json);
+                foreach($data as $rec){
+                  $text= $rec->answer;
+                  //-----------------------
+                }
             }
-        }
         $bot->replyText($reply_token, $text);
     }
 }
