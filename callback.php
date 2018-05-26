5ebc4f92b01a62515cc33eda9aeba5d936f89166
popwandee <popwandee@gmail.com>
2018-05-26 23:32:37 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -10,80 +10,179 @@ echo "Dump temp OK";
 define("LINE_MESSAGING_API_CHANNEL_SECRET", '6f6b7e3b1aff242cd4fb0fa3113f7af3');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN", 'RvsMabRN/IlT2BtmEoH+KcIbha8F/aPLWWzMKj8lxz/7f9c/Ygu5qvrUGtdlrTwyQwR5tFcgIGGzCkHO/SzIKrdCqUm+sal4t73YOuTPZsQX4bR35g3ZJGTvFilxvO1LVO/I6B1ouhx3UjGWe+OwswdB04t89/1O/w1cDnyilFU=');
 echo "ok 1";
+
 require __DIR__."/vendor/autoload.php";
+
 echo "ok 2";
+
 $bot = new \LINE\LINEBot(
+
     new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
+
     ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
+
 );
+
 echo "ok 3";
+
 $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
 $body = file_get_contents("php://input");
+
+
 
 $events = $bot->parseEventRequest($body, $signature);
 
+
+
 foreach ($events as $event) {
+
    if ($event  instanceof \LINE\LINEBot\Event\MessageEvent\ImageMessage){
+
      $reply_token = $event->getReplyToken();
-     $text = 'รูปอะไรเหรอฮะ';
+       $a = ['ว้าว ว้าว ว้าว', 'อุ๊ยตาย ว้ายกรีดดดด', 'ไม่หื่นนะฮะ'];
+
+    $text = $a[mt_rand(0, count($a) - 1)];
+
+     //$text = 'รูปอะไรเหรอฮะ';
+
       $bot->replyText($reply_token, $text);
+
    }
+
     if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+
         $reply_token = $event->getReplyToken();
+
         $text = $event->getText();
+
         $explodeText=explode(" ",$text);
+
         //$bot->replyText($reply_token, $explodeText[0]);
+
         switch ($explodeText[0]) {
+
           case 'สอนฮูก':
+
               //$x_tra = str_replace("สอนฮูก","", $text);
+
               $pieces = explode("|", $explodeText[1]);
+
               $_question=str_replace("[","",$pieces[0]);
+
               $_answer=str_replace("]","",$pieces[1]);
+
               //Post New Data
+
               $newData = json_encode(array('question' => $_question,'answer'=> $_answer) );
+
               $opts = array('http' => array( 'method' => "POST",
+
                                             'header' => "Content-type: application/json",
+
                                             'content' => $newData
+
                                              )
+
                                           );
+
               // เพิ่มเงื่อนไข ตรวจสอบว่ามีข้อมูลในฐานข้อมูลหรือยัง
+
               $api_key="6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv";
+
               $url = 'https://api.mlab.com/api/1/databases/hooqline/collections/linebot?apiKey='.$api_key.'';
+
               $context = stream_context_create($opts);
+
               $returnValue = file_get_contents($url,false,$context);
+
               $text = 'ขอบคุณที่สอนฮูก ฮะ คุณสามารถสอนให้ฉลาดได้เพียงพิมพ์: สอนฮูก [คำถาม|คำตอบ] ต้องเว้นวรรคด้วยนะ  สอบถามราคาหุ้นพิมพ์ stock ถามข่าวพิมพ์ news';
+
               $bot->replyText($reply_token, $text);
+
               break;
+
           case 'stock':
-                 $symbol=$explodeText[1];
-                $text= 'stock price ตรวจสอบราคาหุ้นรายวัน '.$symbol.' click ';
-                $text = $text.'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='.$symbol.'.bk&apikey=W6PVFUDUDT6NEEN1';
+
+                $symbol=$explodeText[1];
+                $text= 'stock price ราคาหุ้นรายวัน '.$symbol.' ';
+                $url_get_data ='https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='.$symbol.'.bk&apikey=W6PVFUDUDT6NEEN1';
+                $content = file_get_contents($url_get_data); // อ่านข้อมูล JSON
+                $jarr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
+                $keepdate = true;
+                $countm= 0;
+                while (list($key) = each($jarr)) { // ทำการ list ค่า key ของ Array ทั้งหมดออกมา
+                  $KeepMainkey = $key; //เก็บคีย์หลัก
+	                 $count = count($jarr[$key]); // นับจำนวนแถวที่เก็บไว้ใน Array ใน key นั้นๆ
+	                 $getarr1 = $jarr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
+                   while (list($key) = each($getarr1)) {
+                     if ($KeepMainkey=="Meta Data" && $countm=='1') {
+    	                  $text= $text."$getarr1[$key]";
+                      }
+                      $countm++;
+                      if ($KeepMainkey!="Meta Data" && $keepdate ) {
+                        $keepdate = false;
+                        $getarrayday = $getarr1[$key];
+                        while (list($key) = each($getarrayday)) {
+                          $text= $text." ".$getarrayday[$key] ; //แสดงคีย์และผลลัพธ์ขอคีย์ของวัน
+                        }//สิ้นสุดการลิสต์คีย์ชั้นลึก (ระดับวัน)
+                      }
+                    }//สิ้นสุดการลิสต์คีย์ชั้นกลาง
+                  } //สิ้นสุดการลิสต์คีย์ชั้นแรก
+
               $bot->replyText($reply_token, $text);
+
               break;
+
           case 'news':
+
               $text=$text.' news update ต่อไปจะรายงานข่าวหุ้นด้วยนะครับ';
+
               $bot->replyText($reply_token, $text);
+
               break;
+
           default:
+
               $api_key="6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv";
+
               $url = 'https://api.mlab.com/api/1/databases/hooqline/collections/linebot?apiKey='.$api_key.'';
+
               $json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$explodeText[0].'"}');
+
               $data = json_decode($json);
+
               $isData=sizeof($data);
+
               if($isData >0){
+
                 foreach($data as $rec){
+
                   $text= $rec->answer;
+
                   $bot->replyText($reply_token, $text);
+
                   //-----------------------
+
                 }//end for each
+
               }else{
+
                   $text='';
+
                   //$text= $explodeText[0];
+
                   //$bot->replyText($reply_token, $text);
+
               }//end no data from mlab
+
             }//end switch
+
     }//end if text
+
 }// end foreach event
+
+
 
 echo "OK4";
 /*
