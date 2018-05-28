a0f505091e6efc46362f0a81b90c8e7b53bb3d4a
popwandee <popwandee@gmail.com>
2018-05-29 00:28:44 +0700

    add condition for only 5 message

--- callback.php
+++ callback.php
@@ -150,6 +150,7 @@ foreach ($events as $event) {
                $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
                while (list($key) = each($json_arr1)) {
                  ++$count_news;
+                 if ($count_news<5)
                     $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].echo$json_arr1[$key]['url'];
                   }
               }
