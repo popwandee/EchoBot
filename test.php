bee4d6ecc22061078a3ead9c40b4db1bf0576918
popwandee <popwandee@gmail.com>
2018-05-28 23:57:06 +0700

    debug

--- test.php
+++ test.php
@@ -22,8 +22,8 @@ $count_news=0;
         }
     }
   }
-  echo '$count_news ='.$count_news;
-  print_r($text_arr);
+  //echo '$count_news ='.$count_news;
+  //print_r($text_arr);
   $random_news_index=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
   $text=$text_arr[$random_news_index];
   echo "last output is ".$text;
