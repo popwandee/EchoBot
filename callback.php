2dfe6ec738511b7120ea06be2fd6e41dcc73313b
popwandee <popwandee@gmail.com>
2018-05-29 01:01:00 +0700

    debug

--- callback.php
+++ callback.php
@@ -142,46 +142,23 @@ foreach ($events as $event) {
 
           case 'news':
           $news_url='https://newsapi.org/v2/top-headlines?country=th&apiKey=dca7d30a57ec451cad6540a696a7f60a' ;
-          $content = file_get_contents($news_url); // อ่านข้อมูล JSON
-          $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
-          $count_news=0;
-            while (list($key) = each($json_arr)) { // ทำการ list ค่า key ของ Array ทั้งหมดออกมา
-              if($key=='articles'){
-               $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
-               while (list($key) = each($json_arr1)) {
-                 ++$count_news;
-                 if ($count_news<5){
-                    $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
-                  }else{ }
-                }
-              }
-            }
-            $text=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
-            $bot->replyText($reply_token, $text_arr);
-
+         $content = file_get_contents($news_url); // อ่านข้อมูล JSON
+         $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
+         $count_news=0;
+           while (list($key) = each($json_arr)) { // ทำการ list ค่า key ของ Array ทั้งหมดออกมา
+             if($key=='articles'){
+              $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
+              while (list($key) = each($json_arr1)) {
+                ++$count_news;
+                   $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
+                 }
+             }
+           }
+           $text=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
+           $bot->replyText($reply_token, $text);
+             break;
 
               break;
-              case 'tel':
-              $news_url='https://newsapi.org/v2/top-headlines?country=th&apiKey=dca7d30a57ec451cad6540a696a7f60a' ;
-              $content = file_get_contents($news_url); // อ่านข้อมูล JSON
-              $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
-              $count_news=0;
-                while (list($key) = each($json_arr)) { // ทำการ list ค่า key ของ Array ทั้งหมดออกมา
-                  if($key=='articles'){
-                   $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
-                   while (list($key) = each($json_arr1)) {
-                     ++$count_news;
-                     if ($count_news<5){
-                        $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
-                      }else{ }
-                    }
-                  }
-                }
-                $text=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
-                $bot->replyText($reply_token, $text_arr);
-
-
-                  break;
 
           default:
 
--- test.php
+++ test.php
@@ -17,13 +17,12 @@ $count_news=0;
           //echo " source: ";echo$json_arr1[$key]['source']['name'];
           //echo " ";echo$json_arr1[$key]['url'];
           //echo "</br>";
-           if($count_news<5){
-             echo $count_news;
-          $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
-        }else{echo "more than/equal 5"}
+
+               $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
+             }
       }
     }
-  }
+
   //echo '$count_news ='.$count_news;
   print_r($text_arr);
   //$text=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
