da40cb8673f8342b1b78c0db2fd0cfa05c7b707b
popwandee <popwandee@gmail.com>
2018-05-29 00:13:23 +0700

    Update callback.php
    add feature Top News Headline

--- callback.php
+++ callback.php
@@ -136,9 +136,27 @@ foreach ($events as $event) {
 
           case 'news':
 
-              $text='https://newsapi.org/v2/top-headlines?country=th&apiKey=dca7d30a57ec451cad6540a696a7f60a' ;
-
-              $bot->replyText($reply_token, $text);
+                $news_url='https://newsapi.org/v2/top-headlines?country=th&apiKey=dca7d30a57ec451cad6540a696a7f60a' ;
+          $content = file_get_contents($news_url); // อ่านข้อมูล JSON
+          $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
+          $count_news=0;
+            while (list($key) = each($json_arr)) { // ทำการ list ค่า key ของ Array ทั้งหมดออกมา
+              if($key=='articles'){
+               $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
+               while (list($key) = each($json_arr1)) {
+                 ++$count_news;
+                    //echo$json_arr1[$key]['title'];
+                    //echo " @ ";echo$json_arr1[$key]['publishedAt'];
+                    //echo$json_arr1[$key]['description'];
+                    //echo$json_arr1[$key]['urlToImage'];
+                    //echo " source: ";echo$json_arr1[$key]['source']['name'];
+                    //echo " ";echo$json_arr1[$key]['url'];
+                    //echo "</br>";
+                    $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];;
+                    $bot->replyText($reply_token, $text_arr[$count_news]);
+                  }
+              }
+            }
 
               break;
 
