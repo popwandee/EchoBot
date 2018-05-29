c34bd630adc9a3a0af5b628086dfe564fc6261bf
popwandee <popwandee@gmail.com>
2018-05-29 09:30:39 +0700

    rollback

--- callback.php
+++ callback.php
@@ -165,38 +165,20 @@ foreach ($events as $event) {
 
           case 'news':
           $news_url='https://newsapi.org/v2/top-headlines?country=th&apiKey=dca7d30a57ec451cad6540a696a7f60a' ;
-         $content = file_get_contents($news_url); // อ่านข้อมูล JSON
-         $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
-         $count_news=0;
-           while (list($key) = each($json_arr)) { // ทำการ list ค่า key ของ Array ทั้งหมดออกมา
-             if($key=='articles'){
-              $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
-              while (list($key) = each($json_arr1)) {
-                $count_news++;
-                if($count_news<5)
-                   $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
-                  else ;
-                 }
-             }
-           }
-           //$text=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
-          //$bot->replyText($reply_token, $text_arr);
-          $response = $bot->replyMessage($event->getReplyToken(), $text_arr);
-/*
-$columns = [];
-for ($i = 0; $i < 5; $i++) {//($i = 0; $i < $max; $i++)
-				$actions = array(
-					new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Add to Card","action=carousel&button="),
-					new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("View",$text_arr[$i]['url'])
-				);
-				$column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder($text_arr[$i]['title'], $text_arr[$i]['description'], $text_arr[$i]['urlToImage'], $actions);
-				$columns[] = $column;
-         }
-         // model Carousel จากอาร์เรย์ของคอลัมน์
-			$carousel_template_builder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
-			$template_message = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Carousel Demo", $carousel_template_builder);
-           $response = $bot->replyMessage($event->getReplyToken(), $template_message);
-*/
+          $content = file_get_contents($news_url); // อ่านข้อมูล JSON
+          $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
+          $count_news=0;
+            while (list($key) = each($json_arr)) { // ทำการ list ค่า key ของ Array ทั้งหมดออกมา
+              if($key=='articles'){
+               $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
+               while (list($key) = each($json_arr1)) {
+                 ++$count_news;
+                    $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
+                  }
+              }
+            }
+            $text=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
+            $bot->replyText($reply_token, $text);
              break;
 
 
