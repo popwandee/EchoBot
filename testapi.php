95a8a23be38a6c9012eab6b1e9b360d905c15b5c
popwandee <popwandee@gmail.com>
2018-06-01 18:53:47 +0700

    test

--- testapi.php
+++ testapi.php
@@ -9,9 +9,10 @@ $json_arr = json_decode($content, true); // แปลงข้อมูล JSON 
 //echo $json_arr[list][0][dt];
 function print_weather($item, $key)
 {
-    echo "$key => $item </br>";
+    echo "$key => $item";
+    echo "<br>";
 }
-array_walk($json_arr, 'print_weather');
+array_walk($json_arr[list], 'print_weather');
 //echo $json_arr[list][0][main][temp_max];
 //echo $json_arr[list][0][main][temp_min];
 //echo $json_arr[list][0][weather][0][main];
