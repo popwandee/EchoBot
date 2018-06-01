80a8bb77fe1521f74150bcc4f195e67b8af89cec
popwandee <popwandee@gmail.com>
2018-06-01 18:51:10 +0700

    test

--- testapi.php
+++ testapi.php
@@ -3,13 +3,13 @@ $explodeText[1]='Bangkok';
 $news_url="http://api.openweathermap.org/data/2.5/forecast?q=".$explodeText[1].",th&units=metric&appid=cb9473cef915ee0ed20ac67817d06289" ;
 $content = file_get_contents($news_url); // อ่านข้อมูล JSON
 $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
-print_r($json_arr);
+//print_r($json_arr);
 //$date=date("F j, Y, g:i a",$json_arr[list][0][dt]);
 //echo $date;
 //echo $json_arr[list][0][dt];
 function print_weather($item, $key)
 {
-    echo "$key => $item\n";
+    echo "$key => $item\n</br>";
 }
 array_walk($json_arr, 'print_weather');
 //echo $json_arr[list][0][main][temp_max];
