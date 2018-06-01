3c16762e86f6e9d85f420a28a8ed6f257d8a1b9f
popwandee <popwandee@gmail.com>
2018-06-01 17:05:49 +0700

    test

--- callback.php
+++ callback.php
@@ -190,6 +190,7 @@ foreach ($events as $event) {
                 break;
 
                 case 'weather':
+                if(is_Null($explodeText[1]))$explodeText[1]="Bangkok";
                $news_url="http://api.openweathermap.org/data/2.5/weather?q=".$explodeText[1].",th&units=metric&appid=cb9473cef915ee0ed20ac67817d06289" ;
                 $content = file_get_contents($news_url); // อ่านข้อมูล JSON
                 $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
