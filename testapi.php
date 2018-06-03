59747baeeabd7dcacb20a888c572161b774e8050
popwandee <popwandee@gmail.com>
2018-06-03 14:38:47 +0700

    test

--- testapi.php
+++ testapi.php
@@ -44,7 +44,7 @@ $count_news=0;
     if($key=='articles'){
      $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
      while (list($key) = each($json_arr1)) {
-       $count_news++;
+       ++$count_news;
        if($count_news<5)
           $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
         }
