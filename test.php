df1eb32f845183ecd3886143ef4320ee22c9514d
popwandee <popwandee@gmail.com>
2018-06-03 12:20:48 +0700

    test

--- test.php
+++ test.php
@@ -13,8 +13,9 @@ for($i=1;$i<$count_element;$i++){
 
 echo $text;
 echo "<br>text_parameter is ".$text_parameter;
+
 $text_parameter="What is it";
-$lang_url="https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=th&dt=t&q=".$text_parameter ;
+$lang_url='https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=th&dt=t&q=$text_parameter' ;
  $content = file_get_contents($lang_url); // อ่านข้อมูล JSON
  $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
 print_r($json_arr);
