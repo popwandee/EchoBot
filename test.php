37d3d88ef88765383c8e016139b7fdea33b87dee
popwandee <popwandee@gmail.com>
2018-06-03 11:59:17 +0700

    text

--- test.php
+++ test.php
@@ -4,11 +4,13 @@ $explodeText=explode(" ",$text);
 $text_parameter='';
 $count_element=count($explodeText);
 print_r($explodeText);
+echo $count_element;
 /*
 foreach($i=1;$i<$count_element;$i++){
   $text_parameter=$text_parameter.$explodeText[$i];
 }
-echo $text;
-echo "<br>".$text_parameter;
 */
+echo $text;
+echo "<br>text_parameter".$text_parameter;
+
 ?>
