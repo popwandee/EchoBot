8743385c6e4b649c9865e831d5c7bca15a89021d
popwandee <popwandee@gmail.com>
2018-06-03 11:57:08 +0700

    test

--- test.php
+++ test.php
@@ -3,8 +3,12 @@ $text="hello yes i do what do you do";
 $explodeText=explode(" ",$text);
 $text_parameter='';
 $count_element=count($explodeText);
+print_r($explodeText);
+/*
 foreach($i=1;$i<$count_element;$i++){
   $text_parameter=$text_parameter.$explodeText[$i];
 }
 echo $text;
 echo "<br>".$text_parameter;
+*/
+?>
