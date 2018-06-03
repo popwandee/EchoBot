346961514bcde180616ee3b05eb938cc84b71806
popwandee <popwandee@gmail.com>
2018-06-03 14:45:56 +0700

    test

--- testapi.php
+++ testapi.php
@@ -34,8 +34,7 @@ try {
 }
 
 
-
-
+$text='';
 $news_url='https://newsapi.org/v2/top-headlines?country=th&apiKey=dca7d30a57ec451cad6540a696a7f60a' ;
 $content = file_get_contents($news_url); // อ่านข้อมูล JSON
 $json_arr = json_decode($content, true); // แปลงข้อมูล JSON ให้อยู่ในรูปแบบ Array
@@ -45,12 +44,12 @@ $count_news=0;
      $json_arr1 = $json_arr[$key]; //ส่งมอบคุณสมบัติ Array ระดับกลาง
      while (list($key) = each($json_arr1)) {
        if($count_news<5)
-          $text_arr[$count_news]=$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
+          $text=$text.$json_arr1[$key]['title'].$json_arr1[$key]['description'].$json_arr1[$key]['url'];
           ++$count_news;
            }
     }
   }
-  //$text=$text_arr[mt_rand(0, count($text_arr) - 1)];//$text_arr[mt_rand[min,max]]; random index
-  //$bot->replyText($reply_token, $text);
-  print_r($text_arr);
+
+
+  print_r($text);
 ?>
