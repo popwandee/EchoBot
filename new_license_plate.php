02add181fefbd46db56381db97d0fd920aa282b5
popwandee <popwandee@gmail.com>
2018-10-08 17:42:26 +0700

    Update new_license_plate.php

--- new_license_plate.php
+++ new_license_plate.php
@@ -82,8 +82,8 @@ try {
     $ownersurname= "จำกัด";
     $ownerid= "105536113550";
     $cartype= "รถยนต์นั่งส่วนบุคคลไม่เกิน 7 คน (รย. 01)";
-    $carbrand= "TOYOTA",
-    $carcolor= "เทา"
+    $carbrand= "TOYOTA";
+    $carcolor= "เทา";
  $newData = json_encode(array('licenseplate' => $licenseplate,'province'=> $province,'username'=> $username,'usersurname'=> $usersurname,'userid'=> $userid,
 			      'ownername'=> $ownername,'ownersurname'=> $ownersurname,'ownerid'=> $ownerid,'cartype'=> $cartype,'carbrand'=> $carbrand,'carcolor'=> $carcolor) );
               $opts = array('http' => array( 'method' => "POST",
@@ -96,17 +96,17 @@ try {
               $returnValue = file_get_contents($url,false,$context);
               if($returnValue) echo "เพิ่มรถ1สำเร็จแล้ว";
               else echo "ไม่สามารถเพิ่มรถ1ได้";
-    $licenseplate= "1กฎ340",
-    $province=  "กรุงเทพมหานคร",
-    $username="อาอีเส๊าะ",
-    $usersurname=  "กาเดร์",
-    $userid= "5-9605-99002-31-2",
-    $ownername="ธนชาต",
-    $ownersurname=  "จำกัด (มหาชน)",
-    $ownerid= 107536001401,
-    $cartype=  "รถยนต์นั่งส่วนบุคคลไม่เกิน 7 คน (รย. 01)",
-    $carbrand= "MITSUBISHI",
-    $carcolor= "ขาว"
+    $licenseplate= "1กฎ340";
+    $province=  "กรุงเทพมหานคร";
+    $username="อาอีเส๊าะ";
+    $usersurname=  "กาเดร์";
+    $userid= "5-9605-99002-31-2";
+    $ownername="ธนชาต";
+    $ownersurname=  "จำกัด (มหาชน)";
+    $ownerid= "107536001401";
+    $cartype=  "รถยนต์นั่งส่วนบุคคลไม่เกิน 7 คน (รย. 01)";
+    $carbrand= "MITSUBISHI";
+    $carcolor= "ขาว";
   $newData = json_encode(array('licenseplate' => $licenseplate,'province'=> $province,'username'=> $username,'usersurname'=> $usersurname,'userid'=> $userid,
 			      'ownername'=> $ownername,'ownersurname'=> $ownersurname,'ownerid'=> $ownerid,'cartype'=> $cartype,'carbrand'=> $carbrand,'carcolor'=> $carcolor) );
               $opts = array('http' => array( 'method' => "POST",
@@ -119,17 +119,17 @@ try {
               $returnValue = file_get_contents($url,false,$context);
               if($returnValue) echo "เพิ่มรถ2สำเร็จแล้ว";
               else echo "ไม่สามารถเพิ่มรถ2ได้";
-   $licenseplate= "1กศ6254",
-    $province=  "กรุงเทพมหานคร",
-    $username= "อุสมาน",
-    $usersurname=  "เจ๊ะแว",
-    $userid= "3-9604-00160-13-3",
+   $licenseplate= "1กศ6254";
+    $province=  "กรุงเทพมหานคร";
+    $username= "อุสมาน";
+    $usersurname=  "เจ๊ะแว";
+    $userid= "3-9604-00160-13-3";
     $ownername= "ทิสโก้",
-    $ownersurname=  "จำกัด(มหาชน)",
-    $ownerid= 107539000171,
-    $cartype=  "รถยนต์นั่งส่วนบุคคลไม่เกิน 7 คน (รย. 01)",
-    $carbrand= "FORD",
-    $carcolor= "เทา"
+    $ownersurname=  "จำกัด(มหาชน)";
+    $ownerid= "107539000171";
+    $cartype=  "รถยนต์นั่งส่วนบุคคลไม่เกิน 7 คน (รย. 01)";
+    $carbrand= "FORD";
+    $carcolor= "เทา";
  $newData = json_encode(array('licenseplate' => $licenseplate,'province'=> $province,'username'=> $username,'usersurname'=> $usersurname,'userid'=> $userid,
 			      'ownername'=> $ownername,'ownersurname'=> $ownersurname,'ownerid'=> $ownerid,'cartype'=> $cartype,'carbrand'=> $carbrand,'carcolor'=> $carcolor) );
               $opts = array('http' => array( 'method' => "POST",
@@ -142,17 +142,17 @@ try {
               $returnValue = file_get_contents($url,false,$context);
               if($returnValue) echo "เพิ่มรถ3สำเร็จแล้ว";
               else echo "ไม่สามารถเพิ่มรถ3ได้";
-    $licenseplate= "1กฮ1631",
-    $province=  "กรุงเทพมหานคร",
-    $username= "มัสตูรา",
-    $usersurname= "อูมา",
-    $userid= "3-9609-00016-12-1",
-    $ownername="ลีสซิ่งกสิกรไทย",
-    $ownersurname=  "จำกัด",
-    $ownerid= 105547166951,
-    $cartype=  "รถยนต์นั่งส่วนบุคคลไม่เกิน 7 คน (รย. 01)",
-    $carbrand= "HONDA",
-    $carcolor= "เทา"
+    $licenseplate= "1กฮ1631";
+    $province=  "กรุงเทพมหานคร";
+    $username= "มัสตูรา";
+    $usersurname= "อูมา";
+    $userid= "3-9609-00016-12-1";
+    $ownername="ลีสซิ่งกสิกรไทย";
+    $ownersurname=  "จำกัด";
+    $ownerid= "105547166951";
+    $cartype=  "รถยนต์นั่งส่วนบุคคลไม่เกิน 7 คน (รย. 01)";
+    $carbrand= "HONDA";
+    $carcolor= "เทา";
   $newData = json_encode(array('licenseplate' => $licenseplate,'province'=> $province,'username'=> $username,'usersurname'=> $usersurname,'userid'=> $userid,
 			      'ownername'=> $ownername,'ownersurname'=> $ownersurname,'ownerid'=> $ownerid,'cartype'=> $cartype,'carbrand'=> $carbrand,'carcolor'=> $carcolor) );
               $opts = array('http' => array( 'method' => "POST",
