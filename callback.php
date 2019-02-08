e6d62f6282ec275116c0d1153cf26f5fb5ca777d
popwandee <popwandee@gmail.com>
2019-02-08 20:58:00 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -374,27 +374,11 @@ case 'news':
                                        $bot->replyText($replyToken, $text);
                                         break;
 	      case '@51':
-		     $replyData=' {  
-  "type": "flex",
-  "altText": "this is a flex message",
-  "contents": {
-    "type": "bubble",
-    "body": {
-      "type": "box",
-      "layout": "vertical",
-      "contents": [
-        {
-          "type": "text",
-          "text": "hello"
-        },
-        {
-          "type": "text",
-          "text": "world"
-        }
-      ]
-    }
-  }
-}';
+		      $altText="this is a flex message";
+		      $flexMessage = new FlexMessageBuilder($altText); ;
+		     
+		     $replyData=$flexMessage;  
+  
 		      break;
           default:
 
