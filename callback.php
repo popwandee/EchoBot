057b36b931244c8edcb8d7f53f1f98aee0342375
popwandee <popwandee@gmail.com>
2019-02-18 10:36:28 +0700

    Update callback.php

--- callback.php
+++ callback.php
@@ -243,7 +243,7 @@ foreach ($events as $event) {
                 foreach($data as $rec){
                   $result= $result.$count.' '.$rec->rank.$rec->name.' '.$rec->lastname.' ('.$rec->position.' '.$rec->deploy_position.') '.$rec->Email.' โทร '.$rec->Tel1." ค่ะ\n\n";
                   if(!is_null($rec->Image)){
-			  $result= $result.$rec->Image;
+			  $result= $result." มีรูปนะครับ";
 		  }else{
 		 	 $imageUrl="https://www.hooq.info/photos/200.jpg";
 		  }
