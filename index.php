589f3f85d74ac087c1fe97bbdc954653f2068265
popwandee <popwandee@gmail.com>
2019-10-20 23:03:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 echo "hello world <br>";
+$userId = "Ua300e9b08826b655e221d12b446d34e5";
+$json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/user_register?apiKey=6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv&q={"userId":"'.$userId.'"}');
+            $data = json_decode($json);
+            $isUserRegister=sizeof($data);
+		if($isUserRegister <=0){
+		   echo "คุณ ยังไม่ได้ลงทะเบียน ID ".$userId." ไม่สามารถเข้าถึงฐานข้อมูลได้นะคะ\n กรุณาพิมพ์ #register ยศ ชื่อ นามสกุล ตำแหน่ง สังกัด หมายเลขโทรศัพท์ เพื่อลงทะเบียนค่ะ";
+                          
+	         }else{ // User registered
+                    
 $nationid='3969900145701';
 $json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/people?apiKey=6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv&q={"nationid":"'.$nationid.'"}');
 
@@ -15,3 +24,4 @@ $data = json_decode($json);
   }else{
     echo "You don't have any data";
   }
+		}//end User registered
