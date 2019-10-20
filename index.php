96d443e69b2eb67386be694dbd68b1fe96a3511d
popwandee <popwandee@gmail.com>
2019-10-20 22:46:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php
 echo "hello world";
+$nationid='3969900145701';
+$json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/people?apiKey=6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv&q={"nationid":"'.$nationid.'"}');
