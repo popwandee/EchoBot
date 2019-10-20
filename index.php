5996f1483383932414d9d602f6848517cc1b739e
popwandee <popwandee@gmail.com>
2019-10-20 22:50:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,11 @@
 echo "hello world";
 $nationid='3969900145701';
 $json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/people?apiKey=6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv&q={"nationid":"'.$nationid.'"}');
+
+$data = json_decode($json);
+  $isData=sizeof($data);
+  if($isData >0){
+    echo "You have data.";
+  }else{
+    echo "You don't have any data";
+  }
