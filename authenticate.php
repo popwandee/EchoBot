933a5b07d5094425d06ac1df8fd78b622163c0f0
popwandee <popwandee@gmail.com>
2019-10-20 23:45:50 +0700

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -5,6 +5,10 @@ if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
 	die ('Please fill both the username and password field!');
 }
+$options = [
+    'cost' => 12,
+];
+echo password_hash("mibn88613", PASSWORD_BCRYPT, $options);
 echo "$_POST password is ".$_POST['password']."<br>";
 	    $json = file_get_contents('https://api.mlab.com/api/1/databases/hooqline/collections/user_register?apiKey=6QxfLc4uRn3vWrlgzsWtzTXBW7CYVsQv&q={"username":"'.$_POST['username'].'"}');
             $data = json_decode($json);
@@ -17,7 +21,7 @@ if ($isUserRegister > 0) {
 	  echo "$password is ".$password."<br>";
       }//end foreach
 	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-	if ($_POST['password'] == $password)) {
+	if (password_verify($_POST['password'],$password)) {
 		// Verification success! User has loggedin!
 		// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
 		session_regenerate_id();
